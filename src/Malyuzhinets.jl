a05e3b868b41a740b21c0a16a7d5f466394fe435
Matthew Priddin <mjp98@cam.ac.uk>
2022-08-10 16:23:04 +0100

    Add specialisation for alpha=1/4

--- src/Malyuzhinets.jl
+++ src/Malyuzhinets.jl
@@ -1,13 +1,19 @@
 module Malyuzhinets
 
-using FastGaussQuadrature, LinearAlgebra
+using FastGaussQuadrature
+using LinearAlgebra
 
-export malyuzhinets_modifiedosipov, malyuzhinets_laguerre
+export malyuzhinets_modifiedosipov
+export malyuzhinets_laguerre
+export malyuzhinets1_4
 
 include("laguerre.jl")
 include("osipov.jl")
 include("aidi.jl")
 include("reduce.jl")
 include("normalise.jl")
+
+# special values
+include("1_4.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,23 @@ using Malyuzhinets
 using Test
 
 @testset "Malyuzhinets.jl" begin
-    tol = 1e-5
     z = 0.5+2im
     alpha = 1/2
-    @test malyuzhinets_modifiedosipov(z,alpha) ≈ malyuzhinets_laguerre(z,alpha,128) atol = tol
+    @test malyuzhinets_modifiedosipov(z,alpha) ≈ malyuzhinets_laguerre(z,alpha,128)
 
+    # Need to go further from the real axis for power series to converge
+    z = 0.5+8im
     alpha = 1/4
-    @test malyuzhinets_modifiedosipov(z,alpha) ≈ malyuzhinets_laguerre(z,alpha,128) atol = tol
+    @test malyuzhinets_modifiedosipov(z,alpha;jmax =200,kmax=200) ≈  malyuzhinets_laguerre(z,alpha,128)
 
+    @test malyuzhinets_modifiedosipov(z,alpha) ≈ malyuzhinets1_4(z) atol = 1e-12
+    z = 0.5+0.2im
+    @test malyuzhinets_laguerre(z,alpha) ≈ malyuzhinets1_4(z) atol = 1e-12
+
+    z = 0.5+8im
     alpha = 1/8
-    @test malyuzhinets_modifiedosipov(z,alpha) ≈ malyuzhinets_laguerre(z,alpha,128) atol = tol
+    @test malyuzhinets_modifiedosipov(z,alpha) ≈ malyuzhinets_laguerre(z,alpha,128)
 
     alpha = 2
-    @test_broken malyuzhinets_modifiedosipov(z,alpha) ≈ malyuzhinets_laguerre(z,alpha,128) atol = tol
+    @test_broken malyuzhinets_modifiedosipov(z,alpha) ≈ malyuzhinets_laguerre(z,alpha,256) atol = tol
 end
